113673c2bc0026f2ff084b3ca28b952ec6acb6a9
Muskan Upadhyay <musupadh@publicisgroupe.net>
2019-04-23 17:28:58 +0530

    Cart Functionality

--- public/js/main.js
+++ public/js/main.js
@@ -30,3 +30,49 @@ $(document).ready(function () {
     mobileMenu("#product-hamburger", ".product-categories", 991);
     mobileMenu("#header-hamburger", ".main-nav", 767);
 });
+
+function addProduct(prodid) {
+    console.log(prodid);
+    //addtocart
+    let xmlReq = new XMLHttpRequest();
+    xmlReq.open("GET", "/cart/addtocart/" + prodid, true);
+    xmlReq.onload = function () {
+        if (xmlReq.status >= 200 && xmlReq.status < 400) {
+            let data = JSON.parse(xmlReq.responseText);
+            console.log(data);
+        } else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+    };
+    xmlReq.onerror = function () {
+        console.log("Connection Error");
+    };
+    xmlReq.send();
+}
+
+function changeProductCount(prodid, operation, input_id) {
+    console.log(prodid);
+    //addtocart
+    let xmlReq = new XMLHttpRequest();
+    xmlReq.open("GET", "/cart/changecartcount/" + prodid + "/" + operation, true);
+    xmlReq.onload = function () {
+        if (xmlReq.status >= 200 && xmlReq.status < 400) {
+            let data = JSON.parse(xmlReq.responseText);
+            console.log(data);
+            data.element.forEach(element => {
+                if (element.id == prodid) {
+                    document.getElementById("prod_count" + input_id).value = element.count;
+                    document.getElementById("prod_price" + input_id).innerHTML = element.price;
+                    document.getElementById("prod_totalprice" + input_id).innerHTML = element.totalprice;
+                    document.getElementById("prod_checkoutprice" + input_id).innerHTML = checkoutAmount;
+                }
+            });
+        } else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+    };
+    xmlReq.onerror = function () {
+        console.log("Connection Error");
+    };
+    xmlReq.send();
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var prodListing = require('../public/data/products/index.get.json');
+var prodSelected = [];
+var checkoutAmount = 0;
 
 /* GET cart page. */
-router.get('/', function(req, res, next) {
-    res.render('cart');
+router.get('/', function (req, res, next) {
+    res.render('cart',{productselect: prodSelected, checkoutAmount: checkoutAmount});
 });
 
+router.get('/addtocart/:id', function (req, res, next) {
+    prodListing.forEach(element => {
+        if (element.id === req.params.id) {
+            if (element.count == undefined) {
+                element.count = 1;
+                element.totalprice = element.price * element.count;
+                checkoutAmount = checkoutAmount + element.price;
+                // element.price = element.price * element.count;
+                prodSelected.push(element);
+            } else {
+                element.count = element.count + 1;
+                element.totalprice = element.price * element.count;
+                checkoutAmount = checkoutAmount + element.price;
+            }       
+        }        
+    });
+    res.end(JSON.stringify({ id: req.params.id, element: prodSelected, checkoutAmount: checkoutAmount}));
+});
+
+router.get('/changecartcount/:id/:operation', function (req, res, next) {
+    if(req.params.operation == "add"){
+        prodSelected.forEach(element => {
+            if (element.id === req.params.id) {
+                element.count = element.count + 1;
+                element.totalprice = element.price * element.count;
+                checkoutAmount = checkoutAmount + element.price;
+            }
+        });
+
+        res.end(JSON.stringify({ id: req.params.id, element: prodSelected, checkoutAmount: checkoutAmount}));
+    }
+    else if(req.params.operation == "remove"){
+        prodSelected.forEach(element => {
+            if (element.id === req.params.id) {
+                element.count = element.count - 1;
+                element.totalprice = element.price * element.count;
+                checkoutAmount = checkoutAmount + element.price;         
+            }
+        });
+        res.end(JSON.stringify({ id: req.params.id, element: prodSelected, checkoutAmount: checkoutAmount}));
+    }
+});
 module.exports = router;
